936759add31ae0f51baa9bfee86c14c0366bed99
Parham Panahi <panahiparham@gmail.com>
2024-06-25 13:59:27 -0600

    feat: added basic implementation for evolve

--- game_of_life/lib.py
+++ game_of_life/lib.py
@@ -47,5 +47,37 @@ class Board:
         return isinstance(value, Board) and np.array_equal(self._board, value._board)
 
     def evolve(self):
-        """FILL ME IN."""
-        pass
+        # loop over a copy of board
+        iter = self._board.copy()
+
+        for i, row in enumerate(iter):
+            for j, cell in enumerate(row):
+                count = self.count_alive_neighbors(iter, i, j)
+                if cell:
+                    if count < 2 or count > 3:
+                        self._board[i, j] = 0
+                else:
+                    if count == 3:
+                        self._board[i, j] = 1
+
+    def count_alive_neighbors(self, board, i, j):
+        count = 0
+
+        i_inc = [-1, 0, 1]
+        j_inc = [-1, 0, 1]
+
+        for id in i_inc:
+            for jd in j_inc:
+                _i = i + id
+                _j = j + jd
+
+                if id == 0 and jd == 0: 
+                    continue
+
+                if _i >= 0 and _i < self._board.shape[0] and _j >= 0 and _j < self._board.shape[1]:
+                    if board[_i, _j]: 
+                        count += 1
+
+        return count
+
+        
